9eb761207f8b9ad5063197da70a8a9ea5d7f5167
demas585 <demas585@mail.ru>
2019-08-09 21:10:34 +0700

    123

--- blogers-league/static/js/main.js
+++ blogers-league/static/js/main.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
     svg4everybody({});
 
+    document.addEventListener(
+        "DOMContentLoaded", () => {
+            const node = document.querySelector( "#mobile-menu" );
+            const menu = new MmenuLight( node, {
+                title: "Меню"
+            } );
+
+            menu.enable( "(max-width: 1300px)" );
+            menu.offcanvas();
+
+            document.querySelector( 'a[href="#mobile-menu"]' )
+                .addEventListener( 'click', ( evnt ) => {
+                    menu.open();
+                    // evnt.preventDefault();
+                    // evnt.stopPropagation();
+                });
+
+            document.querySelector( '.main-nav__item' )
+                .addEventListener( 'click', ( evnt ) => {
+                    menu.close();
+                    // evnt.preventDefault();
+                    // evnt.stopPropagation();
+                });
+        }
+    );
+
+    AOS.init();
 
 });
+
+
